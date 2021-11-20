5def66cebd0dbc25c96095c5d98d25b991e7c577
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-20 18:05:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BST Programs");
+        }
+    }
+}
